717a3a31fc4e7f225499931c59c1319d073a4f32
Srivarsha0110 <162302225+Srivarsha0110@users.noreply.github.com>
2025-09-10 13:29:41 +0530

    Update users-table-init.sql

--- sql_scripts/users-table-init.sql
+++ sql_scripts/users-table-init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE USERS (
     id INT NOT NULL IDENTITY(1, 1),
     username VARCHAR(64) NOT NULL,
-    password_hash VARCHAR(128) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
 INSERT INTO dbo.users (username, password_hash)
-VALUES ('admin', 'pbkdf2:sha256:150000$QlIrz6Hg$5f4cd25d78a6c79906a53f74ef5d3bb2609af2b39d9e5dd6f3beabd8c854dd60')
+VALUES ('cmsadmin', 'scrypt:32768:8:1$GnQAb1ZaFRmIpgRb$9b19eace30bc57acf4475f7a5b9e2bbb02c8380a8e2862a189c9e3632d6de0d7b72cd5b547901a4430b93b1fe7cdae921f675247d50fd5f7cfe09517d6b7e48c')
